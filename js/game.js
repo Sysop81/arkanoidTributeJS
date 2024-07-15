f6ae232d466db7d3d363e473f4d44ab6074cdfa9
José Ramón López <joser.lopez@upct.es>
2024-07-15 17:54:46 +0200

    Add a model folder, menu gameObject and finally update the game class to show menuOptions

--- js/game.js
+++ js/game.js
@@ -3,6 +3,96 @@
  * Autor: José Ramón López Guillén
  */
 
+// Imports libraries
+import { loadLanguage } from "/js/util/tools.js";
+import { Menu } from "/js/model/menu.js";
+
 window.onload = function(){
-    console.log("Game script loaded")
+    
+    /**
+     * initialize
+     * This function initializes the video game
+     */
+    function initialize(){
+        // Step 1. Load language from i18n
+        loadLanguage()
+        .then(data => {
+
+            // Step 2. Load global vars
+            LANGUAGE = data;
+            menu = new Menu(CANVAS,CTX,LANGUAGE);
+            
+            // Step 2. Show the game option menu
+            idGame = setInterval(menu.showGameMenu.bind(menu),frameRate);
+            
+            // Step 3. Handlers
+            document.addEventListener("keydown",manageMenu,false)
+            
+        })
+        .catch(error => {
+            console.error('Error loading language:', error);
+        });
+
+    }
+
+    
+    /**
+     * manageMenu [Handler]
+     * Manage gameMenu option
+     * @param evt
+     */
+    function manageMenu(evt){
+        switch (evt.keyCode) {
+			case 13:
+                // ENTER
+                if(menu.menuSelected == LANGUAGE.OPTION_MENU[0].TITLE){
+                    console.log("Start a new game with dificulty -> " + menu.dificultSelected);
+                }else if (menu.menuSelected == LANGUAGE.OPTION_MENU[1].TITLE){
+                    // GAME MODES
+                    menu.menuIndex = 1;
+                    menu.menuItemIndex = LANGUAGE.DIFICULT.DIFICULT_MODES.findIndex(item => item.TITLE == menu.dificultSelected);
+                    menu.menuSelected = menu.dificultSelected;
+                }else if(menu.menuSelected == LANGUAGE.OPTION_MENU[2].TITLE){
+                    console.log("show rankings")    
+                    menu.menuIndex = 2;
+                }
+                break;
+            case 27:
+                // ESC
+                menu.menuItemIndex = menu.menuIndex    
+                menu.menuSelected = LANGUAGE.OPTION_MENU[menu.menuItemIndex].TITLE;
+                menu.menuIndex = 0;
+                break;
+			case 38:
+                // Up arrow
+                menu.manageOpMenu(false)
+                break;
+            case 40:
+                // Down arrow
+                menu.manageOpMenu(true)
+                break; 
+        }        
+    } 
+
+    
+
+
+    /*********************************************************************                                                                                                                                          *
+	*         FLUJO DEL PROGRAMA                                         *                                                                                                                                         *
+	**********************************************************************/
+    
+    // Global var
+
+    var LANGUAGE = undefined;
+    const CANVAS = document.getElementById("myGameCanvas");
+    let CTX = CANVAS.getContext("2d");
+    let frameRate = 50;
+    let idGame = undefined;
+    let menu = undefined;
+       
+    
+    initialize();    
+    
+    
+
 }
---
+++ js/model/menu.js
@@ -0,0 +1,108 @@
+/**
+ * Class Menu
+ * Represents the game menu options
+ */
+export class Menu{
+    constructor(canvas,ctx,lang){
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.lang = lang;
+        this.menuSelected = lang.OPTION_MENU[0].TITLE;
+        this.dificultSelected = lang.DIFICULT.DIFICULT_MODES[0].TITLE;
+        this.menuIndex = 0;
+        this.menuItemIndex = 0;
+    }
+
+    /**
+     * showGameMenu
+     * Clear canvas && draw de menuItems options
+     */
+    showGameMenu(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawMenuOption()
+    }
+
+    /**
+     * drawMenuOption
+     */
+    drawMenuOption(){
+        
+        this.ctx.font ="24px arcade";   
+		this.ctx.fillStyle="#FFFFFF"; 
+
+        let menuOptions = this.getMenuItems();
+        let height = 250;
+
+        if(this.menuIndex > 0){
+            this.ctx.fillText(this.menuIndex == 1 ? this.lang.DIFICULT.TITLE : this.lang.RANKING.TITLE, 130, height);
+            height += 30;
+        }
+
+        for(let i = 0; i < menuOptions.length;i++){
+            this.ctx.fillText(this.isOpMenuSelected(menuOptions[i].TITLE), 180, height); 
+            height += 30;
+        }
+    }
+
+    /**
+     * isOpMenuSelected
+     * This function format title text adding the character "*" to mark the option selected
+     * @param opMenu 
+     * @returns String
+     */
+    isOpMenuSelected(opMenu){
+        return (opMenu ==  this.menuSelected ? "*  " : "       ") + opMenu;
+    }
+
+    /**
+     * getMenuItems
+     * This function return the array with the menuItems values to show
+     * @returns Array
+     */
+    getMenuItems(){
+        let menuOptions = undefined;
+        switch(this.menuIndex){
+            case 0:
+                menuOptions = this.lang.OPTION_MENU;
+                break;
+            case 1:
+                menuOptions = this.lang.DIFICULT.DIFICULT_MODES;
+                break;
+            case 2: 
+                console.log("Ranking");
+                menuOptions = [];
+                break;            
+        }
+
+        return menuOptions;
+    }
+
+    /**
+     * manageOpMenu
+     * This function manage the item index of menu
+     * @param isDown 
+     */
+    manageOpMenu(isDown){
+        if(isDown){
+            if( this.menuItemIndex >= 2){
+                this.menuItemIndex = 0;
+            }else{
+                this.menuItemIndex++;
+            }
+        }else{
+            if(this.menuItemIndex <= 0){
+                this.menuItemIndex = 2;
+            }else{
+                this.menuItemIndex--;
+            }
+        }
+        
+        this.menuSelected = this.getMenuItems()[this.menuItemIndex].TITLE
+        
+        if(this.menuIndex == 1){
+            this.dificultSelected = this.menuSelected
+            console.log("game modes. Selected : " + this.menuSelected)
+        }
+        
+    }
+}
