2ef8da62e21dd4870b16bce4b725c355a36bf0e8
José Ramón López <jose.rlg27@gmail.com>
2024-08-09 18:08:48 +0200

    Adding capsule and explosion game objects

---
+++ js/model/capsule.js
@@ -0,0 +1,96 @@
+/**
+ * Class Capsule
+ * This class represents the capsule object to get the shot power.
+ */
+export class Capsule{
+    constructor(Xposition,Yposition){
+        this.capsuleSizeX = 44;
+        this.capsuleSizeY = 28;
+        this.x = Xposition;
+        this.y = Yposition;
+        this.positionOfMatrixAnimation = 0;
+        this.isEnabled = true;
+    }
+
+
+    /**
+     * draw
+     * This method draw the capsule in canvas
+     */
+    draw(){
+        this.ctx.drawImage(this.image,                          // Sprite with all types of capsule
+			this.animation[this.positionOfMatrixAnimation][0],  // X position of sprite
+			this.animation[this.positionOfMatrixAnimation][1],	// Y position of sprite
+			this.capsuleSizeX,                                  // Size X
+			this.capsuleSizeY,	                                // Size Y
+			this.x,                                             // Display X position to draw the capsule
+			this.y,	                                            // Display Y position to draw the capsule
+			this.capsuleSizeX,                                  // Size X
+			this.capsuleSizeY);                                 // Size Y
+
+    }
+
+
+    /**
+     * loadAnimation
+     * This method increment the Y position and the sprite position to animate the capsule. 
+     * Later if Y capsule position is grater than canvas height the capsule is destroy 
+     */
+    loadAnimation(){
+        this.positionOfMatrixAnimation = this.positionOfMatrixAnimation < this.animation.length - 1 ? this.positionOfMatrixAnimation + 1 : 0;
+        
+        if(this != undefined && this.y < this.canvas.height){
+            this.draw();
+            this.y ++;
+        }else{
+            this.destroy();
+        }
+    }
+
+
+    /**
+     * destroy
+     * This method clear animation interval and set the capsule instance disabled
+     */
+    destroy(){
+        this.positionOfMatrixAnimation = 0;
+        clearInterval(this.idAnimation);
+        this.idAnimation = undefined;
+        this.isEnabled = false;
+    }
+
+    /**
+    * playGetCapsuleAudio
+    * This method plays the audio of when capsule is collected by the player
+    */
+    playGetCapsuleAudio(){
+        Capsule.prototype.audiogetCapsule.currentTime = 0;
+        Capsule.prototype.audiogetCapsule.play();
+    }
+
+    /**
+     * playCapsuleDescendingAudio
+     * This method plays the capsule descending audio
+     */
+    playCapsuleDescendingAudio(){
+        Capsule.prototype.audioCapsuleDescending.currentTime = 0;
+        Capsule.prototype.audioCapsuleDescending.play();
+    }
+
+    /**
+     * loadCapsuleAsset
+     * Set properties on Capsule class prototype
+     * @param {*} canvas
+     * @param {*} ctx
+     */
+    static loadCapsuleAsset(canvas,ctx){
+        Capsule.prototype.canvas = canvas;
+        Capsule.prototype.ctx = ctx;
+        Capsule.prototype.image = new Image();
+        Capsule.prototype.image.src = "../../assets/img/sprites/shot_capsule.png";
+        Capsule.prototype.animation = [[0,0],[44,0],[88,0],[132,0],[176,0],[220,0],[264,0],[308,0]];
+        Capsule.prototype.idAnimation = undefined;
+        Capsule.prototype.audioCapsuleDescending = document.getElementById("descendingcapsule");
+        Capsule.prototype.audiogetCapsule = document.getElementById("playerCollectCapsule");
+    }
+}
--- js/model/explosion.js
+++ js/model/explosion.js
@@ -3,11 +3,19 @@
  * This class represents the all explosion object to the game.
  */
 export class Explosion{
+    static EXPLOSION_TYPES = {
+        STAR : "STAR",
+        DEATH: "DEATH",
+        SHOT : "SHOT"
+    };
+
     constructor(type,Xposition,Yposition){
-        this.explosionSizeX = 100;
-        this.explosionSizeY = 120;
-        this.positionXsprite = 150
-        this.positionYsprite = 130
+        const OPTIONS = Explosion.#getExplosionSizes(type);
+        this.type = type;
+        this.explosionSizeX = OPTIONS.explosionSizeX
+        this.explosionSizeY = OPTIONS.explosionSizeY
+        this.positionXsprite = OPTIONS.positionXsprite
+        this.positionYsprite = OPTIONS.positionYsprite
         this.x = Xposition;
         this.y = Yposition;
         this.positionOfMatrixAnimation = 0;
@@ -19,9 +27,13 @@ export class Explosion{
      * This method draw the explosion game object on game canvas
      */
     draw(){
-        this.ctx.drawImage(this.imageStar,                        // Sprite with all types of Explosion
-			this.animationStar[this.positionOfMatrixAnimation][0],// Sprite X position to draw
-			this.animationStar[this.positionOfMatrixAnimation][1],// Sprite Y Position yo draw 
+        // Get the draw options in real time
+        const OPTIONS = Explosion.#getDrawOptions(this.type);   
+        
+        // Draw the correct explosion in canvas
+        this.ctx.drawImage(OPTIONS.image,                        // Sprite with all types of Explosion
+			OPTIONS.animation[this.positionOfMatrixAnimation][0],// Sprite X position to draw
+			OPTIONS.animation[this.positionOfMatrixAnimation][1],// Sprite Y Position yo draw 
 			this.positionXsprite,                                 // Size X of the explosion
 			this.positionYsprite,	                              // Size Y of the explosion
 			this.x,                                               // Display X position to draw the explosion
@@ -30,17 +42,34 @@ export class Explosion{
 			this.explosionSizeY);                                 // Size Y of the explosion
     }
 
+    
     /**
-     * loadStarAnimate
+     * loadAnimate
+     * This method calculate the positions of sprite animation on base of the animation type. 
+     * Later draw the game object in canvas and finally reset the animation.
      */
-    loadStarAnimate(){
-        if(this.positionOfMatrixAnimation < this.animationStar.length){
+    loadAnimate(){
+        if(this.positionOfMatrixAnimation < (this.type == Explosion.EXPLOSION_TYPES.STAR ? this.animationStar.length : 
+            this.type == Explosion.EXPLOSION_TYPES.SHOT ? this.animationShot.length : this.animationDeath.length)){
+            // Increment sprite frame && draw in canvas
             this.draw();
             this.positionOfMatrixAnimation++;
         }else{
+            // Reset and clear interval animation
             this.positionOfMatrixAnimation = 0;
-            clearInterval(this.idAnimationStar);
-            this.idAnimationStar = undefined;
+            switch(this.type){
+                case Explosion.EXPLOSION_TYPES.STAR:
+                    clearInterval(this.idAnimationStar);
+                    this.idAnimationStar = undefined;
+                    break;
+                case Explosion.EXPLOSION_TYPES.SHOT:
+                    clearInterval(this.idAnimationShot);
+                    this.animationShot = undefined;
+                    break;
+                default:
+                    clearInterval(this.idAnimationDeath);
+                    this.animationDeath = undefined;
+            }
         }
     }
 
@@ -51,10 +80,77 @@ export class Explosion{
       * @param {*} ctx 
       */
      static loadExplosionAsset(ctx){
+        const PATH = "../../assets/img/sprites/";
         Explosion.prototype.ctx = ctx;
         Explosion.prototype.imageStar = new Image();
-        Explosion.prototype.imageStar.src = "../../assets/img/sprites/star_explosion.png";
+        Explosion.prototype.imageStar.src = `${PATH}star_explosion.png`; 
         Explosion.prototype.animationStar = [[0,0],[154,0],[316,0],[478,0],[0,174],[154,174],[316,174],[478,174],[0,308],[154,308],[316,308],[478,308]];
         Explosion.prototype.idAnimationStar = undefined;
+        Explosion.prototype.imageDeath = new Image();
+        Explosion.prototype.imageDeath.src = `${PATH}death_explosion.png`; 
+        Explosion.prototype.animationDeath = [[0,0],[106,0],[212,0],[325,0],[440,0],
+                                              [0,70],[106,70],[212,70],[325,70],[440,70],
+                                              [0,136],[106,136],[212,136],[325,136],[440,136],
+                                              [0,197],[106,197],[212,197],[325,197],[440,197],
+                                              [0,260],[106,260],[212,260],[325,260],[440,260]];
+        Explosion.prototype.idAnimationDeath = undefined; 
+        Explosion.prototype.imageShot = new Image();
+        Explosion.prototype.imageShot.src = `${PATH}shot_explosion.png`;
+        Explosion.prototype.animationShot = [[0,0],[128,0],[256,0],[384,0],
+                                             [0,126],[128,126],[256,126],[384,126],
+                                             [0,252],[128,252],[256,252],[384,252],
+                                             [0,378],[128,378],[256,378],[384,378]];
+        Explosion.prototype.idAnimationShot = undefined;                                     
+    }
+
+
+    /**
+     * getExplosionSizes [private]
+     * This method returns the animation options besed on the type passed by parameter
+     * @param {*} type 
+     * @returns JSON options
+     */
+    static #getExplosionSizes(type){
+        // All options of animation explosion
+        const OPTIONS = [
+            {
+                type : Explosion.EXPLOSION_TYPES.STAR,
+                explosionSizeX : 100,
+                explosionSizeY : 120,
+                positionXsprite : 150,
+                positionYsprite : 130
+            },
+            {
+                type : Explosion.EXPLOSION_TYPES.SHOT,
+                explosionSizeX : 100,
+                explosionSizeY : 120,
+                positionXsprite : 150,
+                positionYsprite : 130
+            },
+            {
+                type : Explosion.EXPLOSION_TYPES.DEATH,
+                explosionSizeX : 300,
+                explosionSizeY : 300,
+                positionXsprite : 110,
+                positionYsprite : 100
+            }
+
+        ];
+
+        return OPTIONS.filter(item => item.type == type)[0];
+    }
+
+
+    /**
+     * getDrawOptions [private]
+     * This method return the explosion draw options based on the type of explosion in real time.
+     * @param {*} type 
+     * @returns JSON options
+     */
+    static #getDrawOptions(type){
+        return {
+            image : type == Explosion.EXPLOSION_TYPES.STAR ? Explosion.prototype.imageStar : type == Explosion.EXPLOSION_TYPES.SHOT ? Explosion.prototype.imageShot : Explosion.prototype.imageDeath,
+            animation : type == Explosion.EXPLOSION_TYPES.STAR ? Explosion.prototype.animationStar : type == Explosion.EXPLOSION_TYPES.SHOT ? Explosion.prototype.animationShot : Explosion.prototype.animationDeath,
+        }
     }
 }
