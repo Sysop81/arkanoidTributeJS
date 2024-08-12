fccd6a60c93079b933003383182737b2ac4b1db5
José Ramón López <jose.rlg27@gmail.com>
2024-08-12 17:44:42 +0200

    Repair a relative route for i118n

--- js/util/tools.js
+++ js/util/tools.js
@@ -8,8 +8,10 @@
  */
 export async function loadLanguage(){
     const uLanguage = navigator.language || navigator.userLanguage;
-    const i18n = "../../i18n/i18n.json";  // [EXAMPLE FOR MORE i18n language files] -> !uLanguage.startsWith('es') ? "../../i18n/i18n_es.json" : "";
-        
+    //const i18n = "../../i18n/i18n.json";  // [EXAMPLE FOR MORE i18n language files] -> !uLanguage.startsWith('es') ? "../../i18n/i18n_es.json" : "";
+    const basePath = window.location.pathname.replace(/\/$/, "");
+    const i18n = `${basePath}/i18n/i18n.json`;
+
     const response = await fetch(i18n);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
